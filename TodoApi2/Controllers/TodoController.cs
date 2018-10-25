5565a782b582a02914fc69ff2713763949da39c7
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-10-25 16:21:55 +0100

    Add GET and POST actions to controller

---
+++ TodoApi2/Controllers/TodoController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi2.Models;
+
+namespace TodoApi2.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Count() == 0)
+            {
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/todo
+        [HttpGet]
+        public List<TodoItem> GetAll()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        // GET: api/todo/{id}
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById([FromRoute] long id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var item = _context.TodoItems.Find(id);
+
+            if (item == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+
+        // POST: api/todo
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+
+            return CreatedAtAction("GetTodo", new { id = item.Id }, item);
+        }
+    }
+}
